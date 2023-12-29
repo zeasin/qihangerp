ede8f694a459bf968514b3dfd61a4adcecfa4012
启航 <qiliping@yeah.net>
2023-12-29 16:27:23 +0800

    商品功能完善

---
+++ ecerp-api/src/main/java/com/qihang/erp/api/service/impl/GoodsServiceImpl.java
@@ -0,0 +1,96 @@
+package com.qihang.erp.api.service.impl;
+
+import java.util.List;
+import com.zhijian.common.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.qihang.erp.api.mapper.GoodsMapper;
+import com.qihang.erp.api.domain.Goods;
+import com.qihang.erp.api.service.IGoodsService;
+
+/**
+ * 商品管理Service业务层处理
+ * 
+ * @author qihang
+ * @date 2023-12-29
+ */
+@Service
+public class GoodsServiceImpl implements IGoodsService 
+{
+    @Autowired
+    private GoodsMapper goodsMapper;
+
+    /**
+     * 查询商品管理
+     * 
+     * @param id 商品管理主键
+     * @return 商品管理
+     */
+    @Override
+    public Goods selectGoodsById(Long id)
+    {
+        return goodsMapper.selectGoodsById(id);
+    }
+
+    /**
+     * 查询商品管理列表
+     * 
+     * @param goods 商品管理
+     * @return 商品管理
+     */
+    @Override
+    public List<Goods> selectGoodsList(Goods goods)
+    {
+        return goodsMapper.selectGoodsList(goods);
+    }
+
+    /**
+     * 新增商品管理
+     * 
+     * @param goods 商品管理
+     * @return 结果
+     */
+    @Override
+    public int insertGoods(Goods goods)
+    {
+        goods.setCreateTime(DateUtils.getNowDate());
+        return goodsMapper.insertGoods(goods);
+    }
+
+    /**
+     * 修改商品管理
+     * 
+     * @param goods 商品管理
+     * @return 结果
+     */
+    @Override
+    public int updateGoods(Goods goods)
+    {
+        goods.setUpdateTime(DateUtils.getNowDate());
+        return goodsMapper.updateGoods(goods);
+    }
+
+    /**
+     * 批量删除商品管理
+     * 
+     * @param ids 需要删除的商品管理主键
+     * @return 结果
+     */
+    @Override
+    public int deleteGoodsByIds(Long[] ids)
+    {
+        return goodsMapper.deleteGoodsByIds(ids);
+    }
+
+    /**
+     * 删除商品管理信息
+     * 
+     * @param id 商品管理主键
+     * @return 结果
+     */
+    @Override
+    public int deleteGoodsById(Long id)
+    {
+        return goodsMapper.deleteGoodsById(id);
+    }
+}
