6dc98edc7852a391340c48ef0682534cf2d9bd12
启航 <qiliping@yeah.net>
2023-12-29 11:43:20 +0800

    开始供应链功能开发

---
+++ ecerp-api/src/main/java/com/qihang/erp/api/controller/ScmSupplierController.java
@@ -0,0 +1,104 @@
+package com.qihang.erp.api.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.zhijian.common.annotation.Log;
+import com.zhijian.common.core.controller.BaseController;
+import com.zhijian.common.core.domain.AjaxResult;
+import com.zhijian.common.enums.BusinessType;
+import com.qihang.erp.api.domain.ScmSupplier;
+import com.qihang.erp.api.service.IScmSupplierService;
+import com.zhijian.common.utils.poi.ExcelUtil;
+import com.zhijian.common.core.page.TableDataInfo;
+
+/**
+ * 【请填写功能名称】Controller
+ * 
+ * @author qihang
+ * @date 2023-12-29
+ */
+@RestController
+@RequestMapping("/scm/supplier")
+public class ScmSupplierController extends BaseController
+{
+    @Autowired
+    private IScmSupplierService scmSupplierService;
+
+    /**
+     * 查询【请填写功能名称】列表
+     */
+    @PreAuthorize("@ss.hasPermi('scm:supplier:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(ScmSupplier scmSupplier)
+    {
+        startPage();
+        List<ScmSupplier> list = scmSupplierService.selectScmSupplierList(scmSupplier);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出【请填写功能名称】列表
+     */
+    @PreAuthorize("@ss.hasPermi('api:supplier:export')")
+    @Log(title = "【请填写功能名称】", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, ScmSupplier scmSupplier)
+    {
+        List<ScmSupplier> list = scmSupplierService.selectScmSupplierList(scmSupplier);
+        ExcelUtil<ScmSupplier> util = new ExcelUtil<ScmSupplier>(ScmSupplier.class);
+        util.exportExcel(response, list, "【请填写功能名称】数据");
+    }
+
+    /**
+     * 获取【请填写功能名称】详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('api:supplier:query')")
+    @GetMapping(value = "/{id}")
+    public AjaxResult getInfo(@PathVariable("id") Long id)
+    {
+        return success(scmSupplierService.selectScmSupplierById(id));
+    }
+
+    /**
+     * 新增【请填写功能名称】
+     */
+    @PreAuthorize("@ss.hasPermi('api:supplier:add')")
+    @Log(title = "【请填写功能名称】", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody ScmSupplier scmSupplier)
+    {
+        return toAjax(scmSupplierService.insertScmSupplier(scmSupplier));
+    }
+
+    /**
+     * 修改【请填写功能名称】
+     */
+    @PreAuthorize("@ss.hasPermi('api:supplier:edit')")
+    @Log(title = "【请填写功能名称】", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody ScmSupplier scmSupplier)
+    {
+        return toAjax(scmSupplierService.updateScmSupplier(scmSupplier));
+    }
+
+    /**
+     * 删除【请填写功能名称】
+     */
+    @PreAuthorize("@ss.hasPermi('api:supplier:remove')")
+    @Log(title = "【请填写功能名称】", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{ids}")
+    public AjaxResult remove(@PathVariable Long[] ids)
+    {
+        return toAjax(scmSupplierService.deleteScmSupplierByIds(ids));
+    }
+}
