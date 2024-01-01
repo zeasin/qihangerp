4fa13e17c2b204cef2d1a58be0aacef9cf6f038f
Richie <280645618@qq.com>
2024-01-01 13:41:51 +0800

    手动创建订单

--- ecerp-vue/src/views/shop/order/index.vue
+++ ecerp-vue/src/views/shop/order/index.vue
@@ -85,29 +85,7 @@
           size="mini"
           @click="handleAdd"
           v-hasPermi="['shop:order:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['shop:order:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['shop:order:remove']"
-        >删除</el-button>
+        >手动创建订单</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -133,9 +111,9 @@
       <el-table-column label="售后状态" align="center" prop="refundStatus" />
       <el-table-column label="订单状态" align="center" prop="orderStatus" />
       <!-- <el-table-column label="邮费，单位：元" align="center" prop="postage" /> -->
-      <el-table-column label="折扣金额，单位：元" align="center" prop="discountAmount" />
-      <el-table-column label="商品金额，单位：元" align="center" prop="goodsAmount" />
-      <el-table-column label="支付金额，单位：元" align="center" prop="payAmount" />
+      <el-table-column label="折扣金额(元)" align="center" prop="discountAmount" />
+      <el-table-column label="商品金额(元)" align="center" prop="goodsAmount" />
+      <el-table-column label="支付金额" align="center" prop="payAmount" />
       <!-- <el-table-column label="支付时间" align="center" prop="payTime" /> -->
       <el-table-column label="收件人" align="center" prop="receiverName" />
       <el-table-column label="手机号" align="center" prop="receiverPhone" />
@@ -155,17 +133,11 @@
           <el-button
             size="mini"
             type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
+            icon="el-icon-view"
+            @click="handleDetail(scope.row)"
             v-hasPermi="['shop:order:edit']"
-          >修改</el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-delete"
-            @click="handleDelete(scope.row)"
-            v-hasPermi="['shop:order:remove']"
-          >删除</el-button>
+          >详情</el-button>
+          
         </template>
       </el-table-column>
     </el-table>
@@ -178,158 +150,7 @@
       @pagination="getList"
     />
 
-    <!-- 添加或修改店铺订单对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="订单编号" prop="orderNum">
-          <el-input v-model="form.orderNum" placeholder="请输入订单编号" />
-        </el-form-item>
-        <el-form-item label="内部店铺ID" prop="shopId">
-          <el-input v-model="form.shopId" placeholder="请输入内部店铺ID" />
-        </el-form-item>
-        <el-form-item label="订单备注" prop="remark">
-          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
-        <el-form-item label="买家留言信息" prop="buyerMemo">
-          <el-input v-model="form.buyerMemo" placeholder="请输入买家留言信息" />
-        </el-form-item>
-        <el-form-item label="标签" prop="tag">
-          <el-input v-model="form.tag" placeholder="请输入标签" />
-        </el-form-item>
-        <el-form-item label="邮费，单位：元" prop="postage">
-          <el-input v-model="form.postage" placeholder="请输入邮费，单位：元" />
-        </el-form-item>
-        <el-form-item label="折扣金额，单位：元，折扣金额=平台优惠+商家优惠+团长免单优惠金额" prop="discountAmount">
-          <el-input v-model="form.discountAmount" placeholder="请输入折扣金额，单位：元，折扣金额=平台优惠+商家优惠+团长免单优惠金额" />
-        </el-form-item>
-        <el-form-item label="商品金额，单位：元，商品金额=商品销售价格*商品数量-改价金额" prop="goodsAmount">
-          <el-input v-model="form.goodsAmount" placeholder="请输入商品金额，单位：元，商品金额=商品销售价格*商品数量-改价金额" />
-        </el-form-item>
-        <el-form-item label="支付金额，单位：元，支付金额=商品金额-折扣金额+邮费" prop="payAmount">
-          <el-input v-model="form.payAmount" placeholder="请输入支付金额，单位：元，支付金额=商品金额-折扣金额+邮费" />
-        </el-form-item>
-        <el-form-item label="支付时间" prop="payTime">
-          <el-input v-model="form.payTime" placeholder="请输入支付时间" />
-        </el-form-item>
-        <el-form-item label="${comment}" prop="receiverName">
-          <el-input v-model="form.receiverName" placeholder="请输入${comment}" />
-        </el-form-item>
-        <el-form-item label="${comment}" prop="receiverPhone">
-          <el-input v-model="form.receiverPhone" placeholder="请输入${comment}" />
-        </el-form-item>
-        <el-form-item label="${comment}" prop="address">
-          <el-input v-model="form.address" placeholder="请输入${comment}" />
-        </el-form-item>
-        <el-form-item label="${comment}" prop="town">
-          <el-input v-model="form.town" placeholder="请输入${comment}" />
-        </el-form-item>
-        <el-form-item label="${comment}" prop="city">
-          <el-input v-model="form.city" placeholder="请输入${comment}" />
-        </el-form-item>
-        <el-form-item label="${comment}" prop="province">
-          <el-input v-model="form.province" placeholder="请输入${comment}" />
-        </el-form-item>
-        <el-form-item label="${comment}" prop="country">
-          <el-input v-model="form.country" placeholder="请输入${comment}" />
-        </el-form-item>
-        <el-form-item label="订单审核时间" prop="auditTime">
-          <el-input v-model="form.auditTime" placeholder="请输入订单审核时间" />
-        </el-form-item>
-        <el-form-item label="发货时间" prop="shippingTime">
-          <el-input v-model="form.shippingTime" placeholder="请输入发货时间" />
-        </el-form-item>
-        <el-form-item label="快递单号" prop="shippingNumber">
-          <el-input v-model="form.shippingNumber" placeholder="请输入快递单号" />
-        </el-form-item>
-        <el-form-item label="物流公司" prop="shippingCompany">
-          <el-input v-model="form.shippingCompany" placeholder="请输入物流公司" />
-        </el-form-item>
-        <el-divider content-position="center">${subTable.functionName}信息</el-divider>
-        <el-row :gutter="10" class="mb8">
-          <el-col :span="1.5">
-            <el-button type="primary" icon="el-icon-plus" size="mini" @click="handleAddSShopOrderItem">添加</el-button>
-          </el-col>
-          <el-col :span="1.5">
-            <el-button type="danger" icon="el-icon-delete" size="mini" @click="handleDeleteSShopOrderItem">删除</el-button>
-          </el-col>
-        </el-row>
-        <el-table :data="sShopOrderItemList" :row-class-name="rowSShopOrderItemIndex" @selection-change="handleSShopOrderItemSelectionChange" ref="sShopOrderItem">
-          <el-table-column type="selection" width="50" align="center" />
-          <el-table-column label="序号" align="center" prop="index" width="50"/>
-          <el-table-column label="erp系统商品id" prop="goodsId" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.goodsId" placeholder="请输入erp系统商品id" />
-            </template>
-          </el-table-column>
-          <el-table-column label="erp系统商品规格id" prop="specId" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.specId" placeholder="请输入erp系统商品规格id" />
-            </template>
-          </el-table-column>
-          <el-table-column label="商品标题" prop="goodsTitle" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.goodsTitle" placeholder="请输入商品标题" />
-            </template>
-          </el-table-column>
-          <el-table-column label="商品图片" prop="goodsImg" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.goodsImg" placeholder="请输入商品图片" />
-            </template>
-          </el-table-column>
-          <el-table-column label="商品编码" prop="goodsNum" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.goodsNum" placeholder="请输入商品编码" />
-            </template>
-          </el-table-column>
-          <el-table-column label="商品规格" prop="goodsSpec" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.goodsSpec" placeholder="请输入商品规格" />
-            </template>
-          </el-table-column>
-          <el-table-column label="商品规格编码" prop="specNum" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.specNum" placeholder="请输入商品规格编码" />
-            </template>
-          </el-table-column>
-          <el-table-column label="商品单价" prop="goodsPrice" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.goodsPrice" placeholder="请输入商品单价" />
-            </template>
-          </el-table-column>
-          <el-table-column label="子订单金额" prop="itemAmount" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.itemAmount" placeholder="请输入子订单金额" />
-            </template>
-          </el-table-column>
-          <el-table-column label="商品数量" prop="quantity" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.quantity" placeholder="请输入商品数量" />
-            </template>
-          </el-table-column>
-          <el-table-column label="是否赠品0否1是" prop="isGift" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.isGift" placeholder="请输入是否赠品0否1是" />
-            </template>
-          </el-table-column>
-          <el-table-column label="已退货数量" prop="refundCount" width="150">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.refundCount" placeholder="请输入已退货数量" />
-            </template>
-          </el-table-column>
-          <el-table-column label="售后状态 1：无售后或售后关闭，2：售后处理中，3：退款中，4： 退款成功 " prop="refundStatus" width="150">
-            <template slot-scope="scope">
-              <el-select v-model="scope.row.refundStatus" placeholder="请选择售后状态 1：无售后或售后关闭，2：售后处理中，3：退款中，4： 退款成功 ">
-                <el-option label="请选择字典生成" value="" />
-              </el-select>
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
-      </div>
-    </el-dialog>
+    
   </div>
 </template>
 
@@ -437,47 +258,6 @@ export default {
         this.loading = false;
       });
     },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
-    // 表单重置
-    reset() {
-      this.form = {
-        id: null,
-        orderNum: null,
-        shopId: null,
-        remark: null,
-        buyerMemo: null,
-        tag: null,
-        refundStatus: null,
-        orderStatus: null,
-        postage: null,
-        discountAmount: null,
-        goodsAmount: null,
-        payAmount: null,
-        payTime: null,
-        receiverName: null,
-        receiverPhone: null,
-        address: null,
-        town: null,
-        city: null,
-        province: null,
-        country: null,
-        auditTime: null,
-        auditStatus: null,
-        shippingTime: null,
-        shippingNumber: null,
-        shippingCompany: null,
-        createTime: null,
-        createBy: null,
-        updateBy: null,
-        updateTime: null
-      };
-      this.sShopOrderItemList = [];
-      this.resetForm("form");
-    },
     /** 搜索按钮操作 */
     handleQuery() {
       this.queryParams.pageNum = 1;
@@ -496,90 +276,7 @@ export default {
     },
     /** 新增按钮操作 */
     handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加店铺订单";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const id = row.id || this.ids
-      getOrder(id).then(response => {
-        this.form = response.data;
-        this.sShopOrderItemList = response.data.sShopOrderItemList;
-        this.open = true;
-        this.title = "修改店铺订单";
-      });
-    },
-    /** 提交按钮 */
-    submitForm() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          this.form.sShopOrderItemList = this.sShopOrderItemList;
-          if (this.form.id != null) {
-            updateOrder(this.form).then(response => {
-              this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
-          } else {
-            addOrder(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
-          }
-        }
-      });
-    },
-    /** 删除按钮操作 */
-    handleDelete(row) {
-      const ids = row.id || this.ids;
-      this.$modal.confirm('是否确认删除店铺订单编号为"' + ids + '"的数据项？').then(function() {
-        return delOrder(ids);
-      }).then(() => {
-        this.getList();
-        this.$modal.msgSuccess("删除成功");
-      }).catch(() => {});
-    },
-	/** ${subTable.functionName}序号 */
-    rowSShopOrderItemIndex({ row, rowIndex }) {
-      row.index = rowIndex + 1;
-    },
-    /** ${subTable.functionName}添加按钮操作 */
-    handleAddSShopOrderItem() {
-      let obj = {};
-      obj.goodsId = "";
-      obj.specId = "";
-      obj.goodsTitle = "";
-      obj.goodsImg = "";
-      obj.goodsNum = "";
-      obj.goodsSpec = "";
-      obj.specNum = "";
-      obj.goodsPrice = "";
-      obj.itemAmount = "";
-      obj.quantity = "";
-      obj.remark = "";
-      obj.isGift = "";
-      obj.refundCount = "";
-      obj.refundStatus = "";
-      this.sShopOrderItemList.push(obj);
-    },
-    /** ${subTable.functionName}删除按钮操作 */
-    handleDeleteSShopOrderItem() {
-      if (this.checkedSShopOrderItem.length == 0) {
-        this.$modal.msgError("请先选择要删除的${subTable.functionName}数据");
-      } else {
-        const sShopOrderItemList = this.sShopOrderItemList;
-        const checkedSShopOrderItem = this.checkedSShopOrderItem;
-        this.sShopOrderItemList = sShopOrderItemList.filter(function(item) {
-          return checkedSShopOrderItem.indexOf(item.index) == -1
-        });
-      }
-    },
-    /** 复选框选中数据 */
-    handleSShopOrderItemSelectionChange(selection) {
-      this.checkedSShopOrderItem = selection.map(item => item.index)
+      this.$router.push('/shop/order/create');
     },
     /** 导出按钮操作 */
     handleExport() {
