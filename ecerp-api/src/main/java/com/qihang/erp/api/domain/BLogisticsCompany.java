e2bbf2454d335c743bb33e05ed2aeaf06029bd4a
启航 <qiliping@yeah.net>
2024-01-12 16:57:12 +0800

    新增快递公司

---
+++ ecerp-api/src/main/java/com/qihang/erp/api/domain/BLogisticsCompany.java
@@ -0,0 +1,55 @@
+package com.qihang.erp.api.domain;
+
+import com.zhijian.common.annotation.Excel;
+import com.zhijian.common.core.domain.BaseEntity;
+
+/**
+ * 物流公司对象 b_logistics_company
+ * 
+ * @author qihang
+ * @date 2024-01-12
+ */
+public class BLogisticsCompany extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** $column.columnComment */
+    private Long id;
+
+    /** 快递公司 */
+    @Excel(name = "快递公司")
+    private String name;
+
+    /** 快递公司编码 */
+    @Excel(name = "快递公司编码")
+    private String number;
+
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
+
+    public Long getId() 
+    {
+        return id;
+    }
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+    public void setNumber(String number) 
+    {
+        this.number = number;
+    }
+
+    public String getNumber() 
+    {
+        return number;
+    }
+
+}
---
+++ ecerp-api/src/main/java/com/qihang/erp/api/mapper/BLogisticsCompanyMapper.java
@@ -0,0 +1,63 @@
+package com.qihang.erp.api.mapper;
+
+import java.util.List;
+import com.qihang.erp.api.domain.BLogisticsCompany;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 物流公司Mapper接口
+ * 
+ * @author qihang
+ * @date 2024-01-12
+ */
+@Mapper
+public interface BLogisticsCompanyMapper 
+{
+    /**
+     * 查询物流公司
+     * 
+     * @param id 物流公司主键
+     * @return 物流公司
+     */
+    public BLogisticsCompany selectBLogisticsCompanyById(Long id);
+
+    /**
+     * 查询物流公司列表
+     * 
+     * @param bLogisticsCompany 物流公司
+     * @return 物流公司集合
+     */
+    public List<BLogisticsCompany> selectBLogisticsCompanyList(BLogisticsCompany bLogisticsCompany);
+
+    /**
+     * 新增物流公司
+     * 
+     * @param bLogisticsCompany 物流公司
+     * @return 结果
+     */
+    public int insertBLogisticsCompany(BLogisticsCompany bLogisticsCompany);
+
+    /**
+     * 修改物流公司
+     * 
+     * @param bLogisticsCompany 物流公司
+     * @return 结果
+     */
+    public int updateBLogisticsCompany(BLogisticsCompany bLogisticsCompany);
+
+    /**
+     * 删除物流公司
+     * 
+     * @param id 物流公司主键
+     * @return 结果
+     */
+    public int deleteBLogisticsCompanyById(Long id);
+
+    /**
+     * 批量删除物流公司
+     * 
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteBLogisticsCompanyByIds(Long[] ids);
+}
