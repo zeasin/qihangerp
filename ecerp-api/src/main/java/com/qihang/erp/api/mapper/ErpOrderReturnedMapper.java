d9467b6161bb01d05073c4c258571a3372d11992
Richie <280645618@qq.com>
2024-01-13 12:23:11 +0800

    完善售后管理

---
+++ ecerp-api/src/main/java/com/qihang/erp/api/mapper/ErpOrderReturnedMapper.java
@@ -0,0 +1,63 @@
+package com.qihang.erp.api.mapper;
+
+import java.util.List;
+import com.qihang.erp.api.domain.ErpOrderReturned;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 退换货Mapper接口
+ * 
+ * @author qihang
+ * @date 2024-01-13
+ */
+@Mapper
+public interface ErpOrderReturnedMapper 
+{
+    /**
+     * 查询退换货
+     * 
+     * @param id 退换货主键
+     * @return 退换货
+     */
+    public ErpOrderReturned selectErpOrderReturnedById(Long id);
+
+    /**
+     * 查询退换货列表
+     * 
+     * @param erpOrderReturned 退换货
+     * @return 退换货集合
+     */
+    public List<ErpOrderReturned> selectErpOrderReturnedList(ErpOrderReturned erpOrderReturned);
+
+    /**
+     * 新增退换货
+     * 
+     * @param erpOrderReturned 退换货
+     * @return 结果
+     */
+    public int insertErpOrderReturned(ErpOrderReturned erpOrderReturned);
+
+    /**
+     * 修改退换货
+     * 
+     * @param erpOrderReturned 退换货
+     * @return 结果
+     */
+    public int updateErpOrderReturned(ErpOrderReturned erpOrderReturned);
+
+    /**
+     * 删除退换货
+     * 
+     * @param id 退换货主键
+     * @return 结果
+     */
+    public int deleteErpOrderReturnedById(Long id);
+
+    /**
+     * 批量删除退换货
+     * 
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteErpOrderReturnedByIds(Long[] ids);
+}
