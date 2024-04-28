daf65860f78b706072b3811a13d50b74130b92c6
启航 <qiliping@yeah.net>
2024-04-28 18:56:15 +0800

    项目结构整理

---
+++ apis/wei-api/src/main/java/cn/qihangerp/api/App.java
@@ -0,0 +1,13 @@
+package cn.qihangerp.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
